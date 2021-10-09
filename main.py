6f5e6c743aed3ee85928d78c2e59a2e2d35dafe0
Maxim Degunov <Derga13@yandex.ru>
2021-10-09 10:45:34 +0300

    added settings, gitignore

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+import logging
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
+import settings
 
+logging.basicConfig(filename="bot.log", level=logging.INFO)
+
+def greet_user(update, context):
+    print("Сработало")
+    update.message.reply_text("Здарова, отец")
+
+def text_to_me(update, context):
+    text = update.message.text
+    print(text)
+    update.message.reply_text(text)
 
 def main():
 
-    print("Hello world")
+    mybot = Updater(settings.API_KEY, use_context=True)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, text_to_me))
+
+
+    logging.info("Бот стартовал")
+    mybot.start_polling()
+    mybot.idle()
 
 if __name__ == '__main__':
     main()
